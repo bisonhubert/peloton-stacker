c24a1e749688a0c8a8065bdf66535efeafc83519
Bison Hubert <bisonhubert@gmail.com>
2021-07-23 23:04:04 -0700

    Add ride schema

--- peloton/ride.py
+++ peloton/ride.py
@@ -1,17 +1,13 @@
-from graphql_ids import AdrianWilliams
-
-class Ride:
-
-    def __init__(self, graphql_id, instructor_slug):
-        self.graphql_id = graphql_id
-        self.instructor_slug = instructor_slug
-        # some of below can be filled out with a getter from the website using the ID
-        # others are future features
-            # - name
-            # - duration
-            # - slug
-            # - next classes (list of slugs, op)
-            # - instructor (slug)
-            # - id (from website)
-            # - tags (list, for filtering)
-            # - times taken
+# would be good to have a jsonschema when validating this
+{
+        "classes": [
+            # {
+            #     "name": "",
+            #     "slug": "",
+            # },
+            {
+                "name": "",
+                "slug": "",
+            },
+        ]
+}
